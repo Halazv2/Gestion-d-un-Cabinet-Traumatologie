e0aa3b6b64ca6d1529e5ccf2eecaeae791634811
Halazv2 <halaziani2003@gmail.com>
2022-01-14 18:15:04 +0100

    phase4

---
+++ js/dashboard.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body');
+const ajouter_patient = document.querySelector('.ajouter-patient button');
+
+ajouter_patient.addEventListener('click',function(){
+    body.classList.toggle('add');
+})
+
+
---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+const body = document.querySelector('body');
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click',function(){
+    body.classList.toggle('open');
+})
+
